a42e3fe526e7cacafe8f604f0cdf9116150e9631
SHE-COORDINATOR-PROG <andi.wiratmoko@icloud.com>
2026-08-19 15:43:10 +0700

    Add files via upload

---
+++ components/LogoRow.tsx
@@ -0,0 +1,22 @@
+export default function LogoRow({ size = "h-8" }: { size?: string }) {
+  const logos = [
+    { src: "/logos/kpp-mining.png", alt: "KPP Mining" },
+    { src: "/logos/ciss.png", alt: "CISS" },
+    { src: "/logos/asto.png", alt: "ASTO" },
+    { src: "/logos/plant-asto.jpeg", alt: "Plant ASTO" },
+  ];
+
+  return (
+    <div className="flex items-center gap-2 flex-wrap">
+      {logos.map((logo) => (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          key={logo.src}
+          src={logo.src}
+          alt={logo.alt}
+          className={`${size} w-auto object-contain`}
+        />
+      ))}
+    </div>
+  );
+}
