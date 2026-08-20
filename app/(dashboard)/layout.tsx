9e35e9a519c6a25d6ff4b2dfcd6f9edcc1f08985
SHE-COORDINATOR-PROG <andi.wiratmoko@icloud.com>
2026-08-20 09:33:26 +0700

    Add files via upload

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 min-w-0 p-4 sm:p-6 lg:p-8">{children}</main>
+      <main className="flex-1 min-w-0 px-4 sm:px-6 lg:px-8 pb-4 sm:pb-6 lg:pb-8 pt-[68px] lg:pt-8">{children}</main>
     </div>
   );
 }
